a98d092f4942fc016da53af775754898adf3bc96
Alin Manole <alin.manole@yahoo.com>
2021-11-04 17:04:04 +0100

    update 5pm

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import React from 'react'
+import Character from './components/Character'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
+    console.log("constructor")
     super()
     this.state = {
-      characters: []
+      characters: [],
+      favorites:[]
     }
+
+    this.handleFavoriteClick = this.handleFavoriteClick.bind(this)
   }
 
+  componentDidMount() {
+    console.log("component did mount")
+    fetch("https://thronesapi.com/api/v2/Characters")
+      .then(result => result.json())
+      .then(result => {
+        this.setState({ characters: result })
+      })
+  }
 
+  handleFavoriteClick(e){
+    console.log("handle click")
+  }
 
-	render() {
-		return(
-			<h1>Game of thrones</h1>
-		)
-	}
+  render() {
+    console.log("render")
+    return (
+      <div className="container col-12 overflow-hiden">
+        <div className="row m-2 mx-auto p-5">
+        <h1>Game of thrones</h1>
+        {this.state.characters.map((character , index ) => (
+            <Character title={character.title} image={character.imageUrl} handleClick={this.handleFavoriteClick} name={character.fullName} />
+        ))}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
---
+++ src/components/Character.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class Character extends Component {
+    render() {
+        console.log(this.props)
+        return (
+                <div className="col-3 d-flex  flex-column bg-light p-3 border rounded border-1 m-3">
+                        <img className="img-fluid rounded " src={this.props.image} alt={this.props.name}></img>
+                        <h1 className="text-center">{this.props.name}</h1>
+                        <h2 className="text-center">{this.props.title}</h2>
+                </div>
+
+        );
+    }
+}
+
+export default Character;
